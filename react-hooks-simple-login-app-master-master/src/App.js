d0c7b37ffdaffa4defe121ba3fb0b36b58f06b38
Toshirojj <riccardidipasquale@arceus.eu>
2022-04-04 13:24:45 +0200

    commit

---
+++ react-hooks-simple-login-app-master-master/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginWithHooks from './LoginWithHooks';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginWithHooks />
+    </div>
+  );
+}
+
+export default App;
