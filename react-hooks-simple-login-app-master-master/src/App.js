241c4197e8128e707ebb236dc8bc93f27132e011
Toshirojj <riccardidipasquale@arceus.eu>
2022-04-04 13:47:13 +0200

    commit,cookie

--- react-hooks-simple-login-app-master-master/src/App.js
+++ react-hooks-simple-login-app-master-master/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Cookie from './Cookie';
 import LoginWithHooks from './LoginWithHooks';
 
 function App() {
   return (
     <div className="App">
+      <Cookie/>
       <LoginWithHooks />
     </div>
   );
---
+++ react-hooks-simple-login-app-master-master/src/Cookie.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+
+axios.get('//192.168.10.x/session')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+export default function Cookie() {
+  return (
+    <div>
+        {checkCookie()}
+    </div>
+  )
+}
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let session = getCookie("session");
+    if (session != "") {
+      alert("Welcome again " + session);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
+  
